887d93ab7b2969b928eb602ff5ed53a395fa3733
JAYDEEP KUMAR <157721833+jaydeepjatav@users.noreply.github.com>
2025-12-06 11:16:08 +0530

    Implement permutation function in Permutations.java

---
+++ Backtracking/Permutations.java
@@ -0,0 +1,23 @@
+public class permutation{
+    // purmutation function
+    public static void findpermutation(String str,String ans){
+        //base case
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+
+        }
+        //recursion
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            String NewStr = str.substring(0,i) + str.substring(i+1);
+            findpermutation(NewStr, ans+curr);
+        }
+    
+    }
+  //main class
+    public static void main(String[] args){
+        String str = "abcde";
+        findpermutation(str, "");
+    }
+}
