b950bdb12fcca6c91f056d9e61e8ff4bce3db091
JAYDEEP KUMAR <157721833+jaydeepjatav@users.noreply.github.com>
2025-12-15 22:44:28 +0530

    Add Duplicate_par class to validate parentheses
    Implement a method to check for duplicate parentheses in a string.

---
+++ Stack/Duplicate_par.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Duplicate_par{
+
+    public static boolean isvalid(String str){
+        Stack<Character> s = new Stack<>();
+
+        for (int i=0;i<str.length();i++) {
+            char ch = str.charAt(i);
+
+            //closing
+            if(ch == ')'){
+                int count =0;
+                while(s.peek() != '('){
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            //opening,opeartor,operand
+            }else{
+                s.push(ch);
+            }
+        }
+    return false;
+    }
+    public static void main(String[] args){
+        String str = "((a+b))";
+        String str2 = "(a+b)";
+        System.out.println(isvalid(str2));
+    }
+}
